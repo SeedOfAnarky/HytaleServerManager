d2a66f1fa70bbef8cb69a4e632550e5e68c37c43
SeedOfAnarky <SeedOfAnarky@gmail.com>
2026-01-31 03:40:11 -0500

    Add files via upload

---
+++ Start-HytaleManager.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set "ASPNETCORE_URLS=http://localhost:5000"
+set "APP_URL=http://localhost:5000"
+echo Starting HytaleManager...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~dp0HytaleManager.exe' -WorkingDirectory '%~dp0' -WindowStyle Hidden | Out-Null"
+ping 127.0.0.1 -n 2 >nul
+start "" "%APP_URL%"
